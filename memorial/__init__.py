331718984826a4479a17cf481b822ef55b84d148
Patrick Steadman <ptsteadman@gmail.com>
2014-08-03 22:38:39 -0400

    working on click to hold

--- memorial/__init__.py
+++ memorial/__init__.py
@@ -5,7 +5,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('messages', '/')
+    config.add_route('messages', '/memorial')
     config.add_route('socketio_service','/socket.io/*remaining')
     config.scan()
     return config.make_wsgi_app()
