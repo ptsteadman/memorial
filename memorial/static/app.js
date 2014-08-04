4a4c467198e58eba9f867285c94e2c83bf2abc97
Patrick Steadman <ptsteadman@gmail.com>
2014-08-03 23:13:28 -0400

    removed click to hold functionality

--- memorial/static/app.js
+++ memorial/static/app.js
@@ -15,14 +15,6 @@ $(document).ready(function(){
             $message.append("<div class='datetime'>" + message['time'] + "<span class='date'> "  + message['date'] + "</span></div>");
             $message.append("<div class='text'>" + message['text'] + "</div>");
             $message.hide().prependTo("#messages").slideDown(150);
-            $(".message").on('mousedown', function(e){
-                $("#clicked-on").html($(this).html())
-                $("#clicked-on").css("display", "block" );
-                $("#clicked-on").css("right", $(this).position().right );
-                $("#clicked-on").css("left", $(this).position().left );
-                $("#clicked-on").css("top", $(this).position().top - 4);
-                
-            });
         }
         
         if(($("#messages").height() + 36 +  $("#header").height()) >= $(window).height()){
@@ -30,10 +22,6 @@ $(document).ready(function(){
         }
     }
 
-    $(document).mouseup(function(){
-            $("#clicked-on").html("") 
-            $("#clicked-on").css("display", "none");
-    })
     setInterval(pullFromQueue, 330);
 
 })
