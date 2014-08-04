6cd70eae600526f41acdacd738ff936e6ae0d02d
Patrick Steadman <ptsteadman@gmail.com>
2014-08-03 20:23:33 -0400

    working smooth ticker!

--- memorial/static/app.js
+++ memorial/static/app.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
     var messages = io.connect("/message");
+    var queue = Array() 
     var container = Array()
     var MAX_MESSAGES = 20
     messages.on('message', function(data){
         data.forEach(function(message){
-            if (container.length == MAX_MESSAGES){
-                container.pop();
-            }
-            container.unshift(message);
+            queue.push(message);
         });
-        $("#messages").html("");
-        var first = true;
-        container.forEach(function(message){
+    });
+    
+    var pullFromQueue = function(){
+        if (queue.length > 0){
+            message = queue.shift()
             $message = $("<div class='message'></div>");
             $message.append("<div class='datetime'>" + message['time'] + "<span class='date'> "  + message['date'] + "</span>:</div>");
             $message.append("<div class='text'>" + message['text'] + "</div>");
-            $("#messages").append($message);
-        });
-    });
-});
+            $message.hide().prependTo("#messages").slideDown(250);
+        }
+        
+        if(($("#messages").height() + 25 +  $("#header").height()) > $(window).height()){
+            console.log('remove')
+            $(".message:last").fadeOut(300, function(){ $(this).remove(); })
+        }
+    }
+
+    setInterval(pullFromQueue, 250);
+})
