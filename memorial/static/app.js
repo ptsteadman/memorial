913fc3f6966db1231bea1869c1ef0ad073d966be
Patrick Steadman <ptsteadman@gmail.com>
2014-08-03 21:56:12 -0400

    working on click to hold

--- memorial/static/app.js
+++ memorial/static/app.js
@@ -12,16 +12,22 @@ $(document).ready(function(){
         if (queue.length > 0){
             var message = queue.shift()
             $message = $("<div class='message'></div>");
-            $message.append("<div class='datetime'>" + message['time'] + "<span class='date'> "  + message['date'] + "</span>:</div>");
+            $message.append("<div class='datetime'>" + message['time'] + "<span class='date'> "  + message['date'] + "</span></div>");
             $message.append("<div class='text'>" + message['text'] + "</div>");
             $message.hide().prependTo("#messages").slideDown(150);
+            $(".message").on('mousedown', function(e){
+                console.log('clicked')
+                $("#clicked-on").html($(e.target).html())
+                $("#clicked-on").css("left", $(e.target).position().left );
+                $("#clicked-on").css("top", $(e.target).position().top );
+            });
         }
         
         if(($("#messages").height() + 36 +  $("#header").height()) >= $(window).height()){
-            console.log('remove')
             $(".message:last").fadeOut(300, function(){ $(this).remove(); })
         }
     }
 
     setInterval(pullFromQueue, 330);
+
 })
