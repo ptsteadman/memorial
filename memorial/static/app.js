b55fd1103280339007e855a5bce1bfd711363d35
Patrick Steadman <ptsteadman@gmail.com>
2014-08-05 03:14:47 -0400

    more styling

--- memorial/static/app.js
+++ memorial/static/app.js
@@ -9,16 +9,26 @@ $(document).ready(function(){
     });
     
     var pullFromQueue = function(){
+        $(".text").on('mouseenter', function(e){
+            $el = $(e.target);
+            $el.css("max-height", "none");
+            console.log('in');
+            $el.on('mouseleave',function(e){
+                $el.css("max-height", "120px");
+            })
+        });
+
         if (queue.length > 0){
             var message = queue.shift()
             $message = $("<div class='message'></div>");
             $message.append("<div class='datetime'>" + message['time'] + "<span class='date'> "  + message['date'] + "</span></div>");
             $message.append("<div class='text'>" + message['text'] + "</div>");
-            $message.hide().prependTo("#messages").slideDown(330);
+            $message.css({opacity: '0'})
+            $message.hide().prependTo("#messages").slideDown(100).animate({ opacity: '1'},200);
         }
         
         if(($("#messages").height() - 1000 +  $("#header").height()) >= $(window).height()){
-            $(".message:last").fadeOut(300, function(){ $(this).remove(); })
+            $(".message:last").fadeOut(300, function(){ $(this).off();  $(this).remove();  })
         }
     }
 
